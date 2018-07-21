0af24377cfc9afb3157e6de55bdcff38cd6be563
Coodex <coodex@gmail.com>
2018-07-21 23:49:37 +0300

    Initial commit V1.0

---
+++ app/src/main/java/sa/hamah/balotcalc/balotcalc/GameRules.java
@@ -0,0 +1,39 @@
+package sa.hamah.balotcalc.balotcalc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GameRules {
+
+    private int Mode;
+    protected List<Integer> TeamA = new LinkedList<>();
+
+    protected List<Integer> TeamB = new LinkedList<>();
+
+
+
+    public int getMode() {
+        return Mode;
+    }
+
+    public void setMode(int mode) {
+        Mode = mode;
+    }
+
+    private void Start(int GameMode){
+
+        if(GameMode == 1){
+
+
+
+
+        }
+        else if( GameMode == 2){
+
+        }
+
+
+
+    }
+
+}
